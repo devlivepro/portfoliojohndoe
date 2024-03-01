ccbdcc63ef9dc7e30f6c7bba4c005b4fb903c91c
david <tyson.b@hotmail.fr>
2024-03-01 20:34:47 +0100

    Updates header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,20 +2,24 @@ import React from "react";
 
 function Header() {
   return (
+    // Bloc header
     <header className="position-relative">
       <div
         className="position-absolute top-0 start-0 w-100 h-100"
-        style={{ opacity: 0.3 }}>
-      </div>
+        style={{ opacity: 0.3 }}
+      ></div>
 
       <div className="container h-100 d-flex flex-column justify-content-center align-items-center position-relative z-index-1">
         <p class="text-center color-official4 size1">John Doe</p>
-        <p className="text-center color-official4 size2">Junior Web Developer</p>
+        <p className="text-center color-official4 size2">
+          Junior Web Developer
+        </p>
         <a href="#about" className="btn btn-primary btn-jd mt-3">
           Read more
         </a>
       </div>
     </header>
+    // End bloc header
   );
 }
 
